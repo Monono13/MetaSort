9006741e28d11c1fbaaf0afc6c8d85fc4c30fc8b
Luis Cividanes <57505286+Monono13@users.noreply.github.com>
2025-02-15 01:04:21 -0400

    Add files via upload

--- main.rs
+++ main.rs
@@ -1,43 +1,52 @@
-use std::io::*;
+use std::io;
 use std::time::{SystemTime, UNIX_EPOCH};
-use std::{fs, io};
+use std::{fs, thread, time::Duration};
 
-//unixdate-xxxxxxxxxxxx-(filetype)
-
-fn main() -> std::io::Result<()> {
+enum Estate {
+    Escaneando,
+    Renombrando,
+}
+fn main() -> io::Result<()> {
+    let mut estate = Estate::Escaneando;
     let mut input = String::new();
-    println!("enter directory name:");
+    println!("Escriba el nombre del directorio:");
     io::stdin()
         .read_line(&mut input)
-        .expect("error: unable to read user input");
-    println!("directory name is: {}", input);
+        .expect("Error: No se pudo leer el input del usuario");
     let dir_name = input.trim();
     loop {
-        let dirs = fs::read_dir(dir_name).expect("error: unable to read directory");
-        for dir in dirs {
-            let file_path = dir.unwrap().path();
-            let file = file_path.display().to_string();
-            let metadata = fs::metadata(&file)?;
-            if let Ok(created_time) = metadata.created() {
-                if let Ok(duration) = created_time.duration_since(UNIX_EPOCH) {
-                    let file_name = file;
-                    let file_id: Vec<&str> = file_name.split('-').collect();
-                    let unix_duration = duration.as_nanos();
-                    let unix_id = format!("./testdir/unixdate-{unix_duration}");
-                    if file_id[0] != "unixdate" {
-                        fs::rename(&file_name, unix_id)?;
-                    } else {
-                        continue;
+        match estate {
+            Estate::Escaneando => {
+                println!("Estado: Escaneando");
+                thread::sleep(Duration::from_millis(4000));
+                let dirs = fs::read_dir(dir_name).expect("Error: No se encontró el directorio");
+                let mut renamed_files = false;
+                for dir in dirs {
+                    let file_path = dir.unwrap().path();
+                    let file = file_path.display().to_string();
+                    let metadata = fs::metadata(&file)?;
+                    if !file.contains("unixdate") {
+                        if let Ok(created_time) = metadata.created() {
+                            if let Ok(duration) = created_time.duration_since(UNIX_EPOCH) {
+                                let unix_duration = duration.as_nanos();
+                                let unix_id = format!("./{}/unixdate-{}", dir_name, unix_duration);
+                                fs::rename(&file, unix_id)?;
+                                println!("El archivo {} fue renombrado a {}", file, unix_duration);
+                                renamed_files = true;
+                            }
+                        }
                     }
-                    println!("El archivo {file_name} fue creado {unix_duration}");
-                    continue;
-                } else {
-                    println!("Error: El tiempo de creación es anterior a la época Unix.");
-                    break;
                 }
-            } else {
-                println!("No se pudo obtener el tiempo de creación del archivo.");
-                break;
+                if renamed_files {
+                    estate = Estate::Renombrando;
+                } else {
+                    continue;
+                }
+            }
+            Estate::Renombrando => {
+                println!("Estado: Renombrando");
+                thread::sleep(Duration::from_millis(4000));
+                estate = Estate::Escaneando;
             }
         }
     }
